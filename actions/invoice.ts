9cce798bd72ec36f446dbe470f53304a1e366879
Mirzaadr <mirzaadr34@yahoo.com>
2024-12-24 02:49:52 +0700

    create invoice page

---
+++ actions/invoice.ts
@@ -0,0 +1,44 @@
+"use server"
+
+import { prisma } from "@/lib/db";
+import { requireUser } from "@/lib/hooks"
+import { invoiceSchema } from "@/lib/zodSchemas";
+import { parseWithZod } from "@conform-to/zod";
+import { redirect } from "next/navigation";
+
+export async function createInvoice(prevState: any, formData:FormData) {
+  const session = await requireUser();
+
+  const submission = parseWithZod(formData, {
+    schema: invoiceSchema,
+  });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  const data =  await prisma.invoice.create({
+    data: {
+      clientAddress: submission.value.clientAddress,
+      clientEmail: submission.value.clientEmail,
+      clientName: submission.value.clientName,
+      currency: submission.value.currency,
+      date: submission.value.date,
+      dueDate: submission.value.dueDate,
+      fromAddress: submission.value.fromAddress,
+      fromEmail: submission.value.fromEmail,
+      fromName: submission.value.fromName,
+      invoiceItemDescription: submission.value.invoiceItemDescription,
+      invoiceItemQuantity: submission.value.invoiceItemQuantity,
+      invoiceItemRate: submission.value.invoiceItemRate,
+      invoiceName: submission.value.invoiceName,
+      invoiceNumber: submission.value.invoiceNumber,
+      status: submission.value.status,
+      total: submission.value.total,
+      note: submission.value.note,
+      userId: session.id,
+    }
+  });
+
+  return redirect("/dashboard/invoices")
+}
---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,9 @@
+import CreateInvoices from "@/components/dashboard/CreateInvoices";
+
+const InvoiceCreationPage = () => {
+  return (
+    <CreateInvoices />
+  )
+}
+
+export default InvoiceCreationPage;
