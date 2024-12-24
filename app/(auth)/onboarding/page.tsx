e3d43eb8392d894f96233675902b60b6ac4962c4
Mirzaadr <mirzaadr34@yahoo.com>
2024-12-25 02:17:53 +0700

    add background to login page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -22,52 +22,57 @@ const OnBoardingPage = () => {
     shouldRevalidate: "onInput",
   });
   return (
-    <div className='min-h-screen w-screen flex items-center justify-center'>
-      <Card className="max-w-sm mx-auto">
-        <CardHeader>
-          <CardTitle className="text-xl">You are almost finished</CardTitle>
-          <CardDescription>Enter your information to create an account</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form action={action} className="grid gap-y-4" id={form.id} onSubmit={form.onSubmit} noValidate>
-            <div className="flex flex-row gap-4">
-              <div className="flex flex-col gap-2">
-                <Label>First Name</Label>
-                <Input
-                  name={fields.firstName.name}
-                  key={fields.firstName.key}
-                  defaultValue={fields.firstName.value}
-                  placeholder="John"
-                />
-                <p className="text-red-500 text-sm">{fields.firstName.errors}</p>
+    <>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
+      </div>
+      <div className='min-h-screen w-screen flex items-center justify-center'>
+        <Card className="max-w-sm mx-auto">
+          <CardHeader>
+            <CardTitle className="text-xl">You are almost finished</CardTitle>
+            <CardDescription>Enter your information to create an account</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form action={action} className="grid gap-y-4" id={form.id} onSubmit={form.onSubmit} noValidate>
+              <div className="flex flex-row gap-4">
+                <div className="flex flex-col gap-2">
+                  <Label>First Name</Label>
+                  <Input
+                    name={fields.firstName.name}
+                    key={fields.firstName.key}
+                    defaultValue={fields.firstName.value}
+                    placeholder="John"
+                  />
+                  <p className="text-red-500 text-sm">{fields.firstName.errors}</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Label>Last Name</Label>
+                  <Input
+                    name={fields.lastName.name}
+                    key={fields.lastName.key}
+                    defaultValue={fields.lastName.value}
+                    placeholder="Doe"
+                  />
+                  <p className="text-red-500 text-sm">{fields.lastName.errors}</p>
+                </div>
               </div>
               <div className="flex flex-col gap-2">
-                <Label>Last Name</Label>
+                <Label>Address</Label>
                 <Input
-                  name={fields.lastName.name}
-                  key={fields.lastName.key}
-                  defaultValue={fields.lastName.value}
-                  placeholder="Doe"
+                  name={fields.address.name}
+                  key={fields.address.key}
+                  defaultValue={fields.address.value}
+                  placeholder="Chad street 123"
                 />
-                <p className="text-red-500 text-sm">{fields.lastName.errors}</p>
+                <p className="text-red-500 text-sm">{fields.address.errors}</p>
               </div>
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label>Address</Label>
-              <Input
-                name={fields.address.name}
-                key={fields.address.key}
-                defaultValue={fields.address.value}
-                placeholder="Chad street 123"
-              />
-              <p className="text-red-500 text-sm">{fields.address.errors}</p>
-            </div>
 
-            <SubmitButton text="Finished onboarding"/>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
+              <SubmitButton text="Finished onboarding"/>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   )
 }
 
